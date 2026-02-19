5f43ca92c555381af5d74838e7f3b43ae6d20441
mith-sen <mith.senn@gmail.com>
2026-02-19 17:01:45 +0530

    added

--- modules/ai_suggester.py
+++ modules/ai_suggester.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash-lite")
 
 def get_ai_analysis(extracted_text):
     prompt = f"""
